f064153636e6cf27364d9fedb707c98bdc9577b6
JeremyKungu <jeremikungu27@gmail.com>
2022-08-20 20:40:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+let record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},    
+  ]
+
+
+
+
+function superbowlWin(array) {
+    const result = array.find( array => array.result === "W");
+    return !!result ? result.year : undefined;
+
+    
+}
+
+superbowlWin(record);
+
+
+
+
+
+
